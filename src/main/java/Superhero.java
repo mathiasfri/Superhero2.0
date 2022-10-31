195348f9f9bbdde6bbd9d2f47cfe59c717f20810
mathiasfri <mathiasfridthjof@live.dk>
2022-10-31 10:53:24 +0100

    Superhero klasse sat op

--- src/main/java/Superhero.java
+++ src/main/java/Superhero.java
@@ -1,3 +1,56 @@
 public class Superhero
 {
+    private String heroName;
+    private String realName;
+    private String superpower;
+    private int age;
+
+    public Superhero(String heroName, String realName, String superpower, int age)
+    {
+        this.heroName = heroName;
+        this.realName = realName;
+        this.superpower = superpower;
+        this.age = age;
+    }
+
+    public String getHeroName(){
+        return heroName;
+    }
+
+    public void setHeroName(String heroName){
+        this.heroName = heroName;
+    }
+
+    public String getRealName(){
+        return heroName;
+    }
+
+    public void setRealName(String realName){
+        this.realName = realName;
+    }
+
+    public String getSuperpower(){
+        return superpower;
+    }
+
+    public void setSuperpower(String superpower){
+        this.superpower = superpower;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Superheltenavn: " + heroName + "\n" +
+                        "Virkelige navn: " + realName + "\n" +
+                        "Superkraft: " + superpower + "\n" +
+                        "Alder: " + age + "\n";
+    }
 }
