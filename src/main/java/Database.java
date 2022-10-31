ab9907e4b607fd286a67d4121f4d399fab4f1cf2
mathiasfri <mathiasfridthjof@live.dk>
2022-10-31 11:44:05 +0100

    Database opdateret

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -2,12 +2,15 @@ import java.util.ArrayList;
 
 public class Database
 {
-    ArrayList<Superhero> superheroes = new ArrayList<>();
+    private ArrayList<Superhero> superheroes = new ArrayList<>();
 
     public void addSuperhero(String heroName, String realName, String superpower, int age)
     {
         superheroes.add(new Superhero(heroName, realName, superpower, age));
     }
 
-
+    public ArrayList<Superhero> getSuperheroes()
+    {
+        return superheroes;
+    }
 }
